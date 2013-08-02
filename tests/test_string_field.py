a746113206762be0bff576bf5e08a71fa120bce6
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-08-02 10:36:45 +0300

    Fixed test case name

--- tests/test_string_field.py
+++ tests/test_string_field.py
@@ -3,5 +3,5 @@ from wtforms_components import StringField
 from tests import FieldTestCase
 
 
-class TestDateField(FieldTestCase):
+class TestStringField(FieldTestCase):
     field_class = StringField
