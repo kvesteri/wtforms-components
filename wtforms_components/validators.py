5375b1307fd3193807787644460af229666a399d
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-17 11:51:55 +0300

    Refactored email validator, added validators to requirements

--- wtforms_components/validators.py
+++ wtforms_components/validators.py
@@ -1,4 +1,5 @@
-import re
+from __future__ import absolute_import
+from validators import is_email
 from wtforms import ValidationError
 from wtforms.validators import StopValidation
 
@@ -213,15 +214,6 @@ class Email(object):
     :copyright: (c) Django Software Foundation and individual contributors.
     :license: BSD
     """
-    user_regex = re.compile(
-        r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*$"  # dot-atom
-        r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"$)',  # quoted-string
-        re.IGNORECASE)
-    domain_regex = re.compile(
-        r'(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?$)'  # domain
-        # literal form, ipv4 address (SMTP 4.1.3)
-        r'|^\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$',
-        re.IGNORECASE)
     domain_whitelist = ['localhost']
 
     def __init__(self, message=None, whitelist=None):
@@ -230,28 +222,5 @@ class Email(object):
             self.domain_whitelist = whitelist
 
     def __call__(self, form, field):
-        if self.message is None:
-            self.message = field.gettext('Invalid email address.')
-
-        value = field.data or ''
-
-        if not value or '@' not in value:
-            raise ValidationError(self.message)
-
-        user_part, domain_part = value.rsplit('@', 1)
-
-        if not self.user_regex.match(user_part):
-            raise ValidationError(self.message)
-
-        if (not domain_part in self.domain_whitelist and
-                not self.domain_regex.match(domain_part)):
-            # Try for possible IDN domain-part
-            try:
-                domain_part = domain_part.encode('idna').decode('ascii')
-                if not self.domain_regex.match(domain_part):
-                    raise ValidationError(self.message)
-                else:
-                    return
-            except UnicodeError:
-                pass
+        if not is_email(field.data, self.domain_whitelist):
             raise ValidationError(self.message)
