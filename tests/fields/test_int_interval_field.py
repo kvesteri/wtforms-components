51352f65dbe3cc7f70b7c7637c9f292266d3c77c
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-01-14 13:44:04 +0200

    Add more tests for interval fields

---
+++ tests/fields/test_int_interval_field.py
@@ -0,0 +1,68 @@
+from wtforms_components import IntIntervalField, FloatIntervalField
+from wtforms_test import FormTestCase
+from wtforms import Form
+from tests import MultiDict
+
+
+class RangeFieldTestCase(FormTestCase):
+    def init_form(self, **kwargs):
+        class TestForm(Form):
+            interval = self.interval_field(**kwargs)
+
+        self.form_class = TestForm
+        return self.form_class
+
+    def test_valid_intervals(self):
+        form_class = self.init_form()
+        for interval in self.valid_ranges:
+            form = form_class(MultiDict(interval=interval))
+            form.validate()
+            assert len(form.errors) == 0
+
+    def test_invalid_intervals(self):
+        form_class = self.init_form()
+        for interval in self.invalid_ranges:
+            form = form_class(MultiDict(interval=interval))
+            form.validate()
+            assert len(form.errors['interval']) == 1
+
+    def test_field_rendering_when_validation_fails(self):
+        form_class = self.init_form()
+        form = form_class(MultiDict(interval='invalid'))
+        form.validate()
+        assert 'value="invalid"' in str(form.interval)
+
+    def test_converts_empty_strings_to_none(self):
+        form_class = self.init_form()
+        form = form_class(MultiDict(interval=''))
+        assert form.data == {'interval': None}
+
+
+class TestIntIntervalField(RangeFieldTestCase):
+    def setup_method(self, method):
+        self.valid_ranges = [
+            '13 - 14',
+            '13 - 13',
+            '0 - 99',
+            '88'
+        ]
+        self.invalid_ranges = [
+            'abc',
+            '14 - 13',
+        ]
+        self.interval_field = IntIntervalField
+
+
+class TestFloatIntervalField(RangeFieldTestCase):
+    def setup_method(self, method):
+        self.valid_ranges = [
+            '13 - 14',
+            '13 - 13',
+            '0 - 99',
+            '88'
+        ]
+        self.invalid_ranges = [
+            'abc',
+            '14 - 13',
+        ]
+        self.interval_field = FloatIntervalField
