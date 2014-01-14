f8b0b47751861d7e3cc7e54cf3811ecf76a43d86
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-01-14 13:32:28 +0200

    Update SA-Utils dependency

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     install_requires=[
         'WTForms>=1.0.4',
         'SQLAlchemy>=0.8.0',
-        'SQLAlchemy-Utils>=0.22.1',
+        'SQLAlchemy-Utils>=0.23.0',
         'six>=1.4.1',
         'validators>=0.5.0',
         'intervals>=0.2.0'
