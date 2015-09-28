99845fce12837d36678a37dc759074448dd8efe3
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-09-28 09:55:00 +0300

    Add flake8 and isort to requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,10 @@ extras_require = {
         'docutils>=0.10',
         'flexmock>=0.9.7',
         'psycopg2>=2.4.6',
-        'WTForms-Test>=0.1.1'
+        'WTForms-Test>=0.1.1',
+        'flake8>=2.4.0',
+        'isort==3.9.6',
+        'natsort==3.5.6'
     ],
     'babel': ['Babel>=1.3'],
     'arrow': ['arrow>=0.3.4'],
