23401f548d903cd22420f80982f8176b5877b305
Janne Vanhala <jpvanhal@users.noreply.github.com>
2024-11-15 14:40:33 +0200

    Clean up wtforms imports
    Remove the conditional imports for WTForms 2 compatibility. WTForms
    3.1.0 is the minimum supported version.

--- tests/test_if_validator.py
+++ tests/test_if_validator.py
@@ -1,20 +1,16 @@
 from wtforms import Form
+from wtforms.fields import StringField
 from wtforms.validators import DataRequired
 from wtforms_test import FormTestCase
 
 from tests import MultiDict
 from wtforms_components import If
 
-try:
-    from wtforms.fields import TextField
-except ImportError:  # wtforms3
-    from wtforms.fields import StringField as TextField
-
 
 class TestIfValidator(FormTestCase):
     def test_only_validates_if_condition_returns_true(self):
         class MyForm(Form):
-            name = TextField(
+            name = StringField(
                 validators=[
                     If(
                         lambda form, field: False,
@@ -29,7 +25,7 @@ class TestIfValidator(FormTestCase):
 
     def test_encapsulates_given_validator(self):
         class MyForm(Form):
-            name = TextField(
+            name = StringField(
                 validators=[
                     If(
                         lambda form, field: True,
@@ -44,7 +40,7 @@ class TestIfValidator(FormTestCase):
 
     def test_supports_custom_error_messages(self):
         class MyForm(Form):
-            name = TextField(
+            name = StringField(
                 validators=[
                     If(
                         lambda form, field: True,
--- tests/test_read_only_function.py
+++ tests/test_read_only_function.py
@@ -1,19 +1,15 @@
 from wtforms import Form
+from wtforms.fields import StringField
 from wtforms_test import FormTestCase
 
 from tests import MultiDict
 from wtforms_components import read_only
 
-try:
-    from wtforms.fields import TextField
-except ImportError:  # wtforms3
-    from wtforms.fields import StringField as TextField
-
 
 class TestReadOnlyFunction(FormTestCase):
     def test_prevents_value_changing(self):
         class MyForm(Form):
-            name = TextField(default="")
+            name = StringField(default="")
 
         form = MyForm()
         read_only(form.name)
@@ -22,7 +18,7 @@ class TestReadOnlyFunction(FormTestCase):
 
     def test_preserves_previous_value(self):
         class MyForm(Form):
-            name = TextField()
+            name = StringField()
 
         form = MyForm()
         form.name.data = "Previous value"
@@ -32,7 +28,7 @@ class TestReadOnlyFunction(FormTestCase):
 
     def test_prevents_value_population(self):
         class MyForm(Form):
-            name = TextField()
+            name = StringField()
 
         class MyModel:
             pass
